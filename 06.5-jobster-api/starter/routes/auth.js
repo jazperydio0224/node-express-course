2b618f08bff09e4e7be17aaa1996650f8a7b104f
jazperydio0224 <jazper.ydio0224@gmail.com>
2023-05-22 00:28:22 +0800

    commited 06.5-jobster-api final code

--- 06.5-jobster-api/starter/routes/auth.js
+++ 06.5-jobster-api/starter/routes/auth.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const { register, login } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
+const express = require("express");
+const router = express.Router();
+const authenticateUser = require("../middleware/authentication");
+const { register, login, updateUser } = require("../controllers/auth");
+const testUser = require("../middleware/testUser");
 
-module.exports = router
+const rateLimiter = require("express-rate-limit");
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: {
+    msg: "Too many requests from this IP, please try again after 15 minutes",
+  },
+});
+
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
+router.patch("/updateUser", authenticateUser, testUser, updateUser);
+
+module.exports = router;
